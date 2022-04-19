583cb08a1ba249603c97a0c3c632bf2b4e4c6822
Thomaz Castro <103527232+Thomaz-Castro@users.noreply.github.com>
2022-04-19 19:37:47 -0300

    Downloader v2
    agora pode escolher entre baixar o video com qualidade maxima, ou minima

--- Downloader.py
+++ Downloader.py
@@ -1,11 +1,36 @@
 from pytube import YouTube
 
 url = str(input('Insira o link do video: '))
-
 youtube = YouTube(url)
 
+print('-'*35)
 print('Titulo: {}'.format(youtube.title))
+print('')
 
-stream= youtube.streams.get_highest_resolution()
+ex = input('Selecione um formato: \n'
+'[ 1 ] - mp4 (video normal)\n'
+'[ 2 ] - mp3 (apenas audio)\n'
+'Selecione: ')
 
-stream.download()
+if ex == '1':
+    qld = input('escolha a qualidade: \n'
+    '[ 1 ] - Qualidade Maxima\n'
+    '[ 2 ] - Qualidade Minima\n'
+    'Selecione: ')
+
+    if qld == '1':
+        stream = youtube.streams.get_highest_resolution()
+        stream.download()
+        print('Foi')
+    
+    elif qld == '2':
+        stream = youtube.streams.get_lowest_resolution()
+        stream.download()
+        print('Foi, só que ruim')
+    else:
+        print('Opção invalida')
+elif ex == '2':
+    print('Função sendo desenvolvida')
+
+else:
+    print('Opção invalida')
