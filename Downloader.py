804096a7ddfa2e3f6f26e37a54b4fed3c372cd17
Thomaz Castro <103527232+Thomaz-Castro@users.noreply.github.com>
2022-04-19 22:32:19 -0300

    Downloader v3
    Adicionei a opção de baixar em mp3

--- Downloader.py
+++ Downloader.py
@@ -1,5 +1,6 @@
 from pytube import YouTube
-from os import getenv
+from os import getenv, remove
+import moviepy.editor as mp
 
 useerr = getenv("USERNAME")
 
@@ -7,6 +8,8 @@ useerr = getenv("USERNAME")
 
 url = str(input('Insira o link do video: '))
 youtube = YouTube(url)
+
+vdtt = youtube.title
 
 print('')
 print('-'*35)
@@ -17,6 +20,10 @@ ex = input('Selecione um formato: \n'
 '[ 1 ] - mp4 (video normal)\n'
 '[ 2 ] - mp3 (apenas audio)\n'
 'Selecione: ')
+
+print('')
+print('-'*35)
+print('')
 
 if ex == '1':
     qld = input('escolha a qualidade: \n'
@@ -38,7 +45,17 @@ if ex == '1':
     else:
         print('Opção invalida')
 elif ex == '2':
-    print('Função sendo desenvolvida')
+    stream = youtube.streams.get_audio_only()
+    cam = str('C:\\Users\\{}\\Downloads'.format(useerr))
+    stream.download(cam)
+    cc = [cam,'\\' , vdtt,'.mp4']
+    cc = ''.join(cc)
+    clip = mp.AudioFileClip(cc)
+    ccdd = [cam,'\\' , vdtt, '\\' ,  '.mp3']
+    ccdd = ''.join(ccdd)
+    clip.write_audiofile(ccdd)
+    remove(cc)
+    print('Baixado com sucesso!')
 
 else:
     print('Opção invalida')
