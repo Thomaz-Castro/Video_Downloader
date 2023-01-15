f7beed1f0c88f2993aaf25df830a33ee151dd497
Thomaz Castro <103527232+Thomaz-Castro@users.noreply.github.com>
2023-01-15 16:23:02 -0300

    Downloader-exe v5.5
    Executável adicionado

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os"], "includes": ["tkinter", "moviepy.editor", "emoji", "pytube"], "include_files": ["icone.ico"]}
+build_exe_options = {"packages": ["os"], "includes": ["tkinter", "moviepy.editor", "emoji", "pytube"], "include_files": ["icon.ico"]}
 
 # GUI applications require a different base on Windows (the default is for
 # a console application).
@@ -16,5 +16,5 @@ setup(
     version="1.0",
     description="Downloader de videos do Youtube feito por Thomaz Castro",
     options={"build_exe": build_exe_options},
-    executables=[Executable("Downloader.py", base=base, icon="icone.ico")]
+    executables=[Executable("Downloader.py", base=base, icon="icon.ico")]
 )
